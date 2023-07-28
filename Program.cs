b532945cc5a7eba9e2dc1cbb9f3dc86efd73ee2b
Kyle Raymond <kyleraymond@microsoft.com>
2023-07-28 09:34:29 -0500

    updated program

--- Program.cs
+++ Program.cs
@@ -1,41 +1,17 @@
 global using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Orchestration;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapPost("plugins/{pluginName}/invoke/{functionName}", async (HttpContext context, Query query, string pluginName, string functionName) =>
+app.MapPost("plugins/{pluginName}/invoke/{functionName}", async (HttpContext httpContext, Query query, string pluginName, string functionName) =>
         {
             try
             {
-
-                var headers = context.Request.Headers;
-                var endpoint = headers["x-sk-web-app-endpoint"];
-                var model = headers["x-sk-web-app-model"];
-                var key = headers["x-sk-web-app-key"];
-                if (String.IsNullOrEmpty(model) || String.IsNullOrEmpty(key))
-                {
-                    throw new Exception("Missing required headers");
-                }
-
-                IKernel kernel;
-
-                //If endpoint is empty, this could be OpenAI
-                if (String.IsNullOrEmpty(endpoint))
-                {
-                    kernel = new KernelBuilder()
-                        .WithOpenAITextCompletionService(model!, key!)
-                        .Build();
-                }
-                else
-                {
-                    kernel = new KernelBuilder()
-                        .WithAzureTextCompletionService(model!, endpoint!, key!)
-                        .Build();
-                }
+                var kernel = LoadKernel(httpContext);
 
                 var pluginDirectory = "Plugins";
-
-                var plugInFunctions = kernel!.ImportSemanticSkillFromDirectory(pluginDirectory, pluginName);
+                var plugInFunctions = kernel.ImportSemanticSkillFromDirectory(pluginDirectory, pluginName);
 
                 var result = await plugInFunctions[functionName].InvokeAsync(query.Value);
                 SKResponse response = new SKResponse();
@@ -50,4 +26,37 @@ app.MapPost("plugins/{pluginName}/invoke/{functionName}", async (HttpContext con
 
         });
 
+
+
+
 app.Run();
+
+static IKernel LoadKernel(HttpContext context)
+{
+    var headers = context.Request.Headers;
+    string? endpoint = headers["x-sk-web-app-endpoint"];
+    string? model = headers["x-sk-web-app-model"];
+    string? key = headers["x-sk-web-app-key"];
+    if (String.IsNullOrEmpty(model) || String.IsNullOrEmpty(key))
+    {
+        throw new Exception("Missing required headers");
+    }
+    else
+    {
+        IKernel kernel;
+        //If endpoint is empty, this could be OpenAI
+        if (String.IsNullOrEmpty(endpoint))
+        {
+            kernel = new KernelBuilder()
+                .WithOpenAITextCompletionService(model!, key!)
+                .Build();
+        }
+        else
+        {
+            kernel = new KernelBuilder()
+                .WithAzureTextCompletionService(model!, endpoint!, key!)
+                .Build();
+        }
+        return kernel;
+    }
+}
